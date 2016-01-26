82fdd5a2c66f9a2b31dd0e36ed8b1219a42edfe0
bmackattack <brian.francis.mcmahon@gmail.com>
2016-01-26 18:51:07 -0500

    Create payingminimum.py

---
+++ payingminimum.py
@@ -0,0 +1,25 @@
+balance = 4213
+annualInterestRate = 0.2
+monthlyPaymentRate = 0.04
+previousBalance = balance
+
+
+
+numMonths = 12
+monthCounter = 0
+totalPaid = 0
+for monthCounter in range(0,12):
+	monthlyInterestRate = annualInterestRate / 12.0
+	minimumMonthlyPayment = round(monthlyPaymentRate * balance,2)
+	monthlyUnpaidBalance = round(balance - minimumMonthlyPayment,2)
+	previousBalance -= minimumMonthlyPayment
+	balance = round(monthlyUnpaidBalance + (monthlyInterestRate * monthlyUnpaidBalance),2)
+	
+	monthCounter += 1
+    print "Month %r: " % monthCounter
+    print "\rMinimum Monthly Payment: %r" % minimumMonthlyPayment
+    print "\rRemaining Balance: %r" % balance
+    totalPaid += minimumMonthlyPayment
+
+print "\rTotal Paid: %r" % totalPaid
+print "\rRemaining Balance: %r" % balance
